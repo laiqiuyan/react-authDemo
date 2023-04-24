5e88f59478871aab052e8a307a235965dd150a58
laiqiuyan <110175804+laiqiuyan@users.noreply.github.com>
2023-04-24 13:19:02 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-authDemo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-uzohpf)
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+import Home from './Component/Home';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <Home/>
+
+    </div>
+  );
+}
